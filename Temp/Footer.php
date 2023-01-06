fe7324a0833c5a30c98f5238d24f9a2616d00ea6
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-01-06 18:57:18 +0530

    change footer

--- Temp/Footer.php
+++ Temp/Footer.php
@@ -1,4 +1,4 @@
-<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+<footer class="footer footer-center p-4 bg-base-300 text-base-content fixed bottom-0">
   <div>
     <p>Copyright © 2022 - All right reserved by GalaxyStore Industries Ltd</p>
   </div>
