5de6ea781b28181b585197b9a8611362b81e0f75
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-01-08 01:03:35 +0530

    complete profile and edit item pages

--- Backend/editItem.php
+++ Backend/editItem.php
@@ -21,6 +21,7 @@ if (isset($_POST['Update'])) {
     $values["price"] = $item[0]['price'];
     $values["quantity"] = $item[0]['quantity'];
     $values["discount"] = $item[0]['discount'];
+    $values["url"] = $item[0]['url'];
 
 
     //print_r($item);
@@ -36,8 +37,9 @@ if (isset($_POST["Edit"])) {
     $price = htmlspecialchars($_POST['itemPrice']);
     $quntity = htmlspecialchars($_POST['itemQuntity']);
     $dicsount = htmlspecialchars($_POST['itemDiscount']);
+    $url = htmlspecialchars($_POST['itemUrl']);
 
-    $sql = "UPDATE item SET name='$name',price='$price',discount='$dicsount',quantity='$quntity' WHERE  id = $id";
+    $sql = "UPDATE item SET name='$name',price='$price',discount='$dicsount',quantity='$quntity',url='$url' WHERE  id = $id";
 
     if (mysqli_query($conn, $sql)) {
         echo "<script>alert('Update')</script>";
@@ -55,34 +57,105 @@ if (isset($_POST["Edit"])) {
 <?php include '../Backend/Header.php'?>
 
 
-<form action="editItem.php?id=<?php echo $item[0]['id']; ?>" method="POST" class="place-self-center outline outline-offset-2 outline-1">
+<form action="editItem.php?id=<?php echo $item[0]['id']; ?>" method="POST" class="place-self-center">
 
-    <div class="container flex flex-col px-10 py-10 space-y-4 w-96">
-
-        <label>
-            Item Name:
-        </label>
-        <input type="text" name="itemName" value="<?php echo $values["name"]; ?>" class="outline outline-offset-2 outline-1">
-        <label>
-            Item Price:
-        </label>
-        <input type="text" name="itemPrice" value="<?php echo $values["price"]; ?>" class="outline outline-offset-2 outline-1">
-        <label>
-            Item Quantity:
-        </label>
-        <input type="text" name="itemQuntity" value="<?php echo $values["quantity"]; ?>" class="outline outline-offset-2 outline-1">
-        <label>
-            Item Discount:
-        </label>
-        <input type="text" name="itemDiscount" value="<?php echo $values["discount"]; ?>" class="outline outline-offset-2 outline-1">
-        <div class="px-12 place-self-center">
-            <input type="submit" value="Update" class="btn btn-info btn-wide" name="Edit">
-        </div>
-
-    </div>
+    <div class="mx-auto w-4/5">
+			<div class="flex justify-center px-6 my-6">
+				<!-- Row -->
+				<div class="w-full xl:w-3/4 lg:w-11/12 flex gap-5">
+					<!-- Col -->
+					<div
+						class="w-full h-auto hidden lg:block lg:w-5/12 bg-cover rounded-l-lg items-center justify-center"
+					><img src="/Shop/Backend/img/update-item.png" class="mt-10"></div>
+					<!-- Col -->
+					<div class="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+						<h3 class="pt-4 text-2xl text-center mb-3">Update your items</h3>
+						<form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+							<div class="mb-4 md:flex md:justify-between">
+								<div class="mb-4 md:mr-2 md:mb-0">
+									<label class="block mb-2 text-sm font-bold text-gray-700" for="ItemName">
+										Name
+									</label>
+									<input
+										class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="name"
+										type="text"
+                                        value="<?php echo $values["name"]; ?>" 
+                                        name="itemName"
+									/>
+                                    <?php //echo $error["namerr"] ? '<p class="text-xs italic text-red-500">Please enter a name.</p>' : '' ?>
+								</div>
+								<div class="md:ml-2">
+									<label class="block mb-2 text-sm font-bold text-gray-700" for="lastName">
+										price
+									</label>
+									<input
+										class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="price"
+										type="text"
+                                        value="<?php echo $values["price"]; ?>" 
+                                        name="itemPrice"
+									/>
+                                    <?php //echo $error["pricerr"] ? '<p class="text-xs italic text-red-500">Please enter a price.</p>' : '' ?>
+								</div>
+							</div>
+							<div class="mb-4">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+                                    Image URL
+								</label>
+								<input
+									class="w-full px-3 py-2 mb-0 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="url"
+									type="text"
+                                    value="<?php echo $values["url"]; ?>" 
+                                    name="itemUrl"
+								/>
+                                <?php //echo $error["urlerr"] ? '<p class="text-xs italic text-red-500">Please enter a url.</p>' : '' ?>
+							</div>
+							<div class="mb-4 md:flex md:justify-between">
+								<div class="mb-4 md:mr-2 md:mb-0">
+									<label class="block mb-2 mt-1 text-sm font-bold text-gray-700" for="password">
+										Quantity
+									</label>
+									<input
+										class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="quantity"
+										type="text"
+                                        value="<?php echo $values["quantity"]; ?>" 
+                                        name="itemQuntity"
+									/>
+                                    <?php //echo $error["qunerr"] ? '<p class="text-xs italic text-red-500">Please enter quantity.</p>' : '' ?>
+								</div>
+								<div class="md:ml-2">
+									<label class="block mb-2 mt-1 text-sm font-bold text-gray-700" for="c_password">
+                                        Discount
+									</label>
+									<input
+										class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="discount"
+										type="text"
+                                        value="<?php echo $values["discount"]; ?>" 
+                                        name="itemDiscount"
+									/>
+                                    <?php //echo $error["diserr"] ? '<p class="text-xs italic text-red-500">Please enter quantity.</p>' : '' ?>
+								</div>
+							</div>
+							<div class="mb-6 text-center">
+								<button
+									class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+									type="submit"
+                                    name="Edit"
+                                    value="Update"
+								>
+                                    Update the product
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
 
 </form>
-
-
 
 <?php include '../Temp/Footer.php' ?>
--- profile.php
+++ profile.php
@@ -1,38 +1,51 @@
-
 <?php include './Temp/Header.php' ?>
 
 <?php
     include './config/db.php';
     $id = $_SESSION['id'];
 
-    $sql = "SELECT fname,sname,email FROM users WHERE id=$id";
+    $sql = "SELECT fname,sname,email,user_type FROM users WHERE id=$id";
 
     $result = mysqli_query($conn, $sql);
 
     $users= mysqli_fetch_all($result, MYSQLI_ASSOC);
 
     mysqli_free_result($result);
-
 ?>
 
-<!-- 
-Display User Result ]
--->
-<p>
+<!-- Display User details  -->
+<div>
     <?php foreach($users as $user): ?>
 
-        <h2><?php echo $user['fname']; ?></h2>
-        <h2><?php echo $user['sname']; ?></h2>
-        <h2><?php echo $user['email']; ?></h2>
-       
+        <div class="mx-auto right-0 mt-2 mb-4 w-2/5 flex flex-col justify-center">
+                <div class="bg-white rounded overflow-hidden shadow-xl border hover:shadow-2xl">
+                    <div class="text-center p-6 bg-teal-800 border-b">
+                    (<svg aria-hidden="true" role="img" class="h-24 w-24 text-white rounded-full mx-auto" width="32" height="32" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="currentColor" d="M172 120a44 44 0 1 1-44-44a44 44 0 0 1 44 44Zm60 8A104 104 0 1 1 128 24a104.2 104.2 0 0 1 104 104Zm-16 0a88 88 0 1 0-153.8 58.4a81.3 81.3 0 0 1 24.5-23a59.7 59.7 0 0 0 82.6 0a81.3 81.3 0 0 1 24.5 23A87.6 87.6 0 0 0 216 128Z"></path></svg>
+                    <p class="pt-2 text-lg font-semibold text-gray-50"><?php echo $user['fname']." ".$user['sname'] ?> </p>
+                    <p class="text-sm text-gray-100 mb-2"><?php echo $user['email'] ?></p>
+                    <p class="text-sm text-gray-100"><?php echo $user['user_type'] ?></p>
+                    <div class="mt-5">
+                        <a
+                        class="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
+                        >
+                        Manage your Account
+                        </a>
+                    </div>
+                    </div>
+
+                    <form action="index.php" method="POST">
+                    <div class="flex items-center">
+                        <button class="w-full px-4 py-2 pb-4 hover:bg-gray-100 flex flex-col items-center" type="submit" value="Logout" name="Logout" id="logout">
+                            <p class="text-sm font-medium text-gray-800 leading-none"><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class="h-4 w-4 text-gray-800 fill-current animate-spin" width="32" height="32" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1024 1024"><path fill="currentColor" d="M988 548c-19.9 0-36-16.1-36-36c0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 0 0-94.3-139.9a437.71 437.71 0 0 0-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7c26.7 63.1 40.2 130.2 40.2 199.3c.1 19.9-16 36-35.9 36z"></path></svg> Logout
+                                    </p>
+                        </button>
+                    </div>
+                    </form>
+                </div>
+            </div>
     
 
     <?php endforeach; ?>
-</p>
-
-
-
-
-
+</div>
 
 <?php include './Temp/footer.php' ?>
